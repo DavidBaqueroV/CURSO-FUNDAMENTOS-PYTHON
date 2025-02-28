075e17422e5f43526392e18ddad83ac07cfd4694
davidbaquero <davidsanti.programador@gmail.com>
2025-02-27 22:24:17 -0500

    solucion actividad 4

---
+++ Fundamentos del Lenguaje/solucion4.py
@@ -0,0 +1,5 @@
+#DESARROLLADO POR: DAVID SANTIAGO BAQUERO VILLARRAGA
+
+frase = input("Ingrese una frase: ")
+num_palabras = len(frase.split())
+print(f"Número de palabras en la frase: {num_palabras}")
