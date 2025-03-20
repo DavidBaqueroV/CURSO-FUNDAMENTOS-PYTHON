e8170e7608a1efadaee5945545d97f34c014c0dc
David_Baquero <davidsanti.programador@gmail.com>
2025-03-20 13:44:49 -0500

    _

--- Fundamentos del Lenguaje/solucion2.py
+++
@@ -1,7 +0,0 @@
-# DESARROLLADO POR: DAVID SANTIAGO BAQUERO VILLARRAGA
-
-# SOLUCION PUNTO 1
-
-# Dibujaremos la P de python
-
-print("PPPP \nP   P\nP   P\nPPPP \nP    \nP    \nP    ")
--- Fundamentos del Lenguaje/solucion5.py
+++
@@ -1,5 +0,0 @@
-#DESARROLLADO POR: DAVID SANTIAGO BAQUERO VILLARRAGA
-
-frase = input("Ingrese una frase: ")
-resultado = frase.upper()[::-1]
-print(resultado)
